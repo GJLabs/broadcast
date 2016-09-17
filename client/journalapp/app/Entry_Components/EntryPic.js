7f9ed160e807541b5d7d58a9a5a09b226ea5a5ae
Sean Kinzler <seankinzler@Seans-MacBook-Air-2.local>
2016-09-16 19:28:09 -0700

    changing entry pic into class

--- client/journalapp/app/Entry_Components/EntryPic.js
+++ client/journalapp/app/Entry_Components/EntryPic.js
@@ -12,22 +12,34 @@ import {
   Image
 } from 'react-native';
 
-var EntryPic = (props) => {
-  if (props.thumbnail === null) {
-    return (
-      <Text>
-      </Text>
-    );
-  } else {
-    return (
-      <View>
-        <Image 
-        style={styles.entryImage}
-        source={{ uri: props.thumbnail }}
-        />
-      </View>
-    )
+export default class EntryPic extends Component {
+
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.state = {};
+  }
+
+  expandPic () {
+
+  } 
+
+  render() {
+    if (props.thumbnail === null) {
+      return (
+        <Text>
+        </Text>
+      );
+    } else {
+      return (
+        <View>
+          <Image 
+          style={styles.entryImage}
+          source={{ uri: props.thumbnail }}
+          onPress={ this.expandPic }
+          />
+        </View>
+      )
+    }
   }
 }
-
-module.exports = EntryPic;
